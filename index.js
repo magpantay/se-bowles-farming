077246f53029886d4055ecd3a5fdf544e5a07714
AlecKwiat <akwiatkowski@ucmerced.edu>
2019-04-25 11:54:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,27 @@ function tableRowCreate(tableclass, rowid)
   console.log("Created new row in " + tableclass + " with " + numCols + " columns");
 }
 
+function openTab(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
 function changeTableText(tableclass, rowNum, colNum, text)
 //changes the text on a table of user-defined class and the (row,col) coordinates of that table
 {
