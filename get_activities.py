2cecc289890151ded9ffbae634a524b7bf33edf6
artymiss <artyspam@gmail.com>
2019-05-03 15:50:46 -0700

    Merged alec's calls

--- get_activities.py
+++ get_activities.py
@@ -1,4 +1,4 @@
-`from requests import get as web_get 	#requests.get(), for CURL requests
+from requests import get as web_get 	#requests.get(), for CURL requests
 from json import loads as json_parser 	#json.loads(), for parsing CURL response
 import csv
 
@@ -18,10 +18,25 @@ def main():
 	while len(each_result['data']) > 0:
 		print ("Writing page {0} of data to '{1}', please wait...".format(counter, fileOut))
 		for i in range(len(each_result['data'])): # inside the loop, we take the current chunk of agworld data and fetch the important bits and save it to a txt file (the fileOut above)
+			#Duplicates, see which we need or if they are just the same.
 			outfile.write("Activity ID: {0}\n".format(each_result['data'][i]['id']))
+			outfile.write("Activity ID [job_activities]: {0}\n".format(each_result['data'][i]['attributes']['job_activity']['activity_id']))
+			outfile.write("Activity Type [attributes]: {0}\n".format(each_result['data'][i]['attributes']['activity_type']))
+			outfile.write("Activity Type [job_activities]: {0}\n".format(each_result['data'][i]['attributes']['job_activity']['activity_type']))
+
 			outfile.write("Task Name: {0}\n".format(each_result['data'][i]['attributes']['title']))
 			outfile.write("Due At: {0}\n".format(each_result['data'][i]['attributes']['due_at']))
 			outfile.write("Completed At: {0}\n".format(each_result['data'][i]['attributes']['completed_at']))
+
+			#Type of Category
+			outfile.write("Activity ID: {0}\n".format(each_result['data'][i]['activity_category']))
+			#------Unsure on these
+			#Unique ID of the Field
+			outfile.write("Field ID: {0}\n".format(each_result['data'][i]['attributes']['activity_fields']['field_id']))
+			#Unique Name of the Field
+			outfile.write("Field Name: {0}\n".format(each_result['data'][i]['attributes']['activity_fields']['field_name']))
+			#Who ordered the Activity
+			outfile.write("Author User Name: {0}\n".format(each_result['data'][i]['attributes']['job_activities']['author_user_name']))
 
 			print ("Foreman: {0}\n".format(each_result['data'][i]['attributes']['operator_users'][0]['name']))
 			for j in range(len(each_result['data'][i]['attributes']['activity_fields'])): # because apparantly a single assignment can involve multiple fields/farms
