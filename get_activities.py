f6d56967881d32ef41b82026825c7c94e93ce513
Kyran Magpantay <magpantay.kyran@gmail.com>
2019-05-07 14:06:02 -0700

    Changed get_activities output to CSV instead of TXT
    - For organization/cleanliness/parsability (is that a word?) reasons
    - Changed behavior of NO-DUE-DATE-SET to use information from AgWorld + the NO-DUE-DATE-SET string to notify user that we can't tell if it's late or on-time
    - Uploading resulting CSV file from the Python script
    
    - CSV was needed for JS parsing

--- get_activities.py
+++ get_activities.py
@@ -8,11 +8,11 @@ from requests import get as web_get 			#requests.get(), for CURL requests || Cur
 from json import loads as json_parser 			#json.loads(), for parsing CURL response || Current call syntax: json_parser()
 from dateutil.parser import parse as date_parse # dateutil.parser.parse(), for converting str from AgWorld to date objects || Current call syntax: date_parse()
 from datetime import datetime					# for getting current system time (datetime.datetime.now()), also needed for time comparison and astimezone (tz conversion for mismatched tz timestamps) || Current call syntax: datetime.now(), obj.astimezone()
-import csv
+import csv										# for the creation of fileOut CSV file and editing of fileCSV CSV file. || Current call syntax:
 from dateutil import tz							# for getting timezone, to convert datetimes into different timezones (tz.gettz()) || Current call syntax: tz.gettz()
 
 def main():
-	fileOut = "things.txt"
+	fileOut = "things.csv"
 	fileCSV = "bowles_farming_table.csv"
 
 	counter = 1
@@ -20,23 +20,22 @@ def main():
 	response = web_get('https://us.agworld.co/user_api/v1/activities?api_token=wFdJRAHjwzylncYDdwrcKw&page[number]={0}&page[size]=100'.format(counter), headers={"Content-Type":"application/vnd.api+json", "Accept":"application/vnd.api+json"}) # gets 100 item chunks at a time
 	each_result = json_parser(response.text)
 
-	outfile = open(fileOut, "w")
-	#outCSV = open(fileCSV, "w")
+	outfile = csv.writer(open(fileOut, "w"), delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
 
-	#what the following loop does is that while the length of result['data'] > 0, then keep fetching from agworld (because we need to continuously fetch 100 [the maximum allowed at a time, also hence why page[size]=100] at a time until each_result['data']'s length is 0 (aka is []))
-	status = "" # will hold complete/incomplete/late/etc.
+	#outCSV = open(fileCSV, "w")
 	pacifictz = tz.gettz("US/Pacific")
 
-	while len(each_result['data']) > 0:
-		print ("Writing page {0} of data to '{1}', please wait...".format(counter, fileOut))
-		for i in range(len(each_result['data'])): # inside the loop, we take the current chunk of agworld data and fetch the important bits and save it to a txt file (the fileOut above)
-			#Duplicates, see which we need or if they are just the same.
-			outfile.write("Activity ID: {0}\n".format(each_result['data'][i]['id']))
-			outfile.write("Activity Type [attributes]: {0}\n".format(each_result['data'][i]['attributes']['activity_type']))
+	outfile.writerow(["Activity ID", "Activity Type [attrib]", "Task Name", "Due At", "Completed At", "Farm Name", "Field Name", "Field ID", "Job Status"])
 
-			outfile.write("Task Name: {0}\n".format(each_result['data'][i]['attributes']['title']))
-			outfile.write("Due At: {0}\n".format(each_result['data'][i]['attributes']['due_at'])) # outputs "None" (type: NoneType) if it doesn't exist, string otherwise
-			outfile.write("Completed At: {0}\n".format(each_result['data'][i]['attributes']['completed_at']))
+	while len(each_result['data']) > 0: #what the following loop does is that while the length of result['data'] > 0, then keep fetching from agworld (because we need to continuously fetch 100 [the maximum allowed at a time, also hence why page[size]=100] at a time until each_result['data']'s length is 0 (aka is []))
+		print ("Writing page {0} of data to '{1}', please wait...".format(counter, fileOut))
+		for i in range(len(each_result['data'])): # inside the loop, we take the current chunk of agworld data and fetch the important bits and save it to a CSV file
+			activity_id = each_result['data'][i]['id']
+			activity_type_attrib = each_result['data'][i]['attributes']['activity_type']
+			task_name = each_result['data'][i]['attributes']['title']
+			due_at = each_result['data'][i]['attributes']['due_at']
+			completed_at = each_result['data'][i]['attributes']['completed_at']
+			status = "" # will hold complete-on-time/complete-late/incomplete-late/etc. based on time comparisons
 
 			#-------------------------------------------------------------------------------------------------------
 			# the list of things that we've tried that some things in the JSON file have, but not all (as in, exists in some not in all. It's lack of existence breaks this program though)
@@ -49,9 +48,9 @@ def main():
 			#outfile.write("Author User Name: {0}\n".format(each_result['data'][i]['attributes']['job_activities']['author_user_name']))
 			#outfile.write ("Operator Name: {0}\n".format(each_result['data'][i]['attributes']['operator_users'][0]['name']))
 			#-------------------------------------------------------------------------------------------------------
+
 			# if the thing is none, apparently it's type NoneType (not string lol)
 			# need .__name__ because that gets the name of type alone rather than < class 'TYPE' >
-
 			if type(each_result['data'][i]['attributes']['due_at']).__name__ != "NoneType" and type(each_result['data'][i]['attributes']['completed_at']).__name__ != "NoneType": # if due date isn't none and completed at isn't none, then completed?? ||If there is a Due Date and a Complete Date, task_status = complete
 				status = "COMPLETE"
 				dtDueAt = date_parse(each_result['data'][i]['attributes']['due_at'])
@@ -65,9 +64,6 @@ def main():
 				else:
 					status = status + "-LATE"
 
-				# compare time comparison to append Status
-				# if complete > due then status.append("-LATE")
-				# else status.append("-ON-TIME")
 			elif type(each_result['data'][i]['attributes']['due_at']).__name__ != "NoneType" and type(each_result['data'][i]['attributes']['completed_at']).__name__ == "NoneType": # if due isn't none but completed is then in progress || If there is a Due Date but NO Complete Date, task_status = in progress
 				status = "IN-PROGRESS"
 				dtDueAt = date_parse(each_result['data'][i]['attributes']['due_at'])
@@ -81,19 +77,18 @@ def main():
 				else:
 					status = status + "-LATE"
 
-				# if currentTime > due then status.append("-LATE")
-				# else don't append anything
 			else: #else there isn't a due date set
-				status = "NO-DUE-DATE-SET"
+				status = each_result['data'][i]['attributes']['job_status'].replace(" ", "-").upper() # makes the AgWorld data look like the format we have for job status by replacing spaces with - and making the entire string uppercase
+				status = status + "-NO-DUE-DATE-SET" # we fetch the status of the activity according to AgWorld, but also add that fact that there was no due date set, notifying that we can't tell if it's late or on-time
 
 			for j in range(len(each_result['data'][i]['attributes']['activity_fields'])): # because apparantly a single assignment can involve multiple fields/farms
-				outfile.write("Farm Name: {0}\n".format(each_result['data'][i]['attributes']['activity_fields'][j]['farm_name'])) # ex. Delta Ranch
-				outfile.write("Field Name: {0}\n".format(each_result['data'][i]['attributes']['activity_fields'][j]['field_name'])) # ex. D-06
-				outfile.write("Field ID: {0}\n".format(each_result['data'][i]['attributes']['activity_fields'][j]['field_id'])) # ex. 416057
+				farm_name = each_result['data'][i]['attributes']['activity_fields'][j]['farm_name']
+				field_name = each_result['data'][i]['attributes']['activity_fields'][j]['field_name']
+				field_id = each_result['data'][i]['attributes']['activity_fields'][j]['field_id']
 
-			outfile.write("Job Status: {0}\n".format(status))#each_result['data'][i]['attributes']['job_status']))
-			# outputs the current status based on status sting
-			outfile.write("\n")
+				outfile.writerow([activity_id, activity_type_attrib, task_name, due_at, completed_at, farm_name, field_name, field_id, status])
+				# this will write a row for each farm name as some tasks involve multiple farms (and it seemed easier to do this for easier CSV file parsing)
+				# it works out since we already have all of the other things to write to the CSV file
 
 		print ("Wrote page {0} to '{1}'".format(counter, fileOut))
 		counter = counter + 1	# to get the next 100 or so tasks
